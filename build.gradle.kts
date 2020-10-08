2fa3ac2005a7f04ebcea74bb8f9ae9b30df29949
Nick Badal <me@nbad.al>
2020-10-08 11:32:12 -0700

    Update dependencies, remove IntelliJ C.O.C.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,11 @@ plugins {
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
     id("org.jetbrains.intellij") version "0.5.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "0.6.0"
+    id("org.jetbrains.changelog") version "0.6.1"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
     id("io.gitlab.arturbosch.detekt") version "1.14.1"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "9.4.0"
+    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
 // Import variables from gradle.properties file
