c2bc6129afb8d57b3ff40ce0b22ed2e1f63dd670
Nick Badal <me@nbad.al>
2022-03-27 18:31:40 -0700

    Fix shell script warnings

--- check-release.sh
+++ check-release.sh
@@ -2,7 +2,7 @@
 set -euf -o pipefail
 
 LATEST_BUILD=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=IIU&release.type=eap%2Crc&latest=true' | jq -r '.IIU[0].build')
-LATEST=$(echo $LATEST_BUILD | cut -d . -f 1)
+LATEST=$(echo "$LATEST_BUILD" | cut -d . -f 1)
 UNTIL="${LATEST}.*"
 
 CURRENT=$(grep pluginUntilBuild gradle.properties | cut -d '=' -f 2 | xargs)
@@ -16,6 +16,5 @@ fi
 echo "::set-output name=updated::true"
 echo "::set-output name=newUntil::$UNTIL"
 
-sed -i -E "s|(pluginVerifierIdeVersions = )(.*)\$|\1\2, $LATEST_BUILD|g" gradle.properties
-sed -i "s|pluginUntilBuild = $CURRENT|pluginUntilBuild = $UNTIL|g" gradle.properties
-
+gsed -i -E "s|(pluginVerifierIdeVersions = )(.*)\$|\1\2, $LATEST_BUILD|g" gradle.properties
+gsed -i "s|pluginUntilBuild = $CURRENT|pluginUntilBuild = $UNTIL|g" gradle.properties
