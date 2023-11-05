d229e88b7eaff538125e68b7aad21ce636db3947
Paul Dingemans <pdingemans@bol.com>
2023-11-05 20:23:33 +0100

    Fix Ktlint format after global find and replace
    When global find and replace is invoked, this also triggers the ActionOnSave. It seems that the global find and replace does change the "document.text" but not the "psiFile.text" before the save is completed. If KtlintFormat then uses the "psiFile.text" (e.g. the original code) it undoes the change made by the global find and replace.

--- plugin/src/main/kotlin/com/nbadal/ktlint/KtlintFormat.kt
+++ plugin/src/main/kotlin/com/nbadal/ktlint/KtlintFormat.kt
@@ -86,7 +86,10 @@ private fun executeKtlintFormat(
                     // The psiFile may contain unsaved changes. So create a snippet based on content of the psiFile *and*
                     // with the same path as that psiFile so that the correct '.editorconfig' is picked up by ktlint.
                     Code.fromSnippet(
-                        content = psiFile.text,
+                        // Get the content via the PsiDocumentManager instead of from "psiFile.text" directly. In case
+                        // the content of an active editor window is changed via a global find and replace, the document
+                        // text is updated but the Psi (and PsiFile) have not yet been changed.
+                        content = PsiDocumentManager.getInstance(project).getDocument(psiFile)!!.text,
                         // TODO: de-comment when parameter is supported in Ktlint 1.1.0
                         // path = psiFile.virtualFile.toNioPath(),
                     ),
