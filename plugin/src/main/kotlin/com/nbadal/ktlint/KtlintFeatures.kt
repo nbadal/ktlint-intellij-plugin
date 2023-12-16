7170a6e2c77ae339065d1a7ce278341b4b44111c
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2023-12-16 17:47:16 +0100

    Use feature toggle for post formatting with ktlint after normal format

--- plugin/src/main/kotlin/com/nbadal/ktlint/KtlintFeatures.kt
+++ plugin/src/main/kotlin/com/nbadal/ktlint/KtlintFeatures.kt
@@ -3,6 +3,7 @@ package com.nbadal.ktlint
 import com.nbadal.ktlint.KtlintFeature.AUTOMATICALLY_DISPLAY_BANNER_WITH_NUMBER_OF_VIOLATIONS_FOUND
 import com.nbadal.ktlint.KtlintFeature.AUTOMATICALLY_DISPLAY_PROBLEM_WITH_NUMBER_OF_VIOLATIONS_FOUND
 import com.nbadal.ktlint.KtlintFeature.FORMAT_WITH_KTLINT_ON_SAVE
+import com.nbadal.ktlint.KtlintFeature.POST_FORMAT_WITH_KTLINT
 import com.nbadal.ktlint.KtlintFeature.SHOW_MENU_OPTION_FORMAT_WITH_KTLINT
 
 /**
@@ -21,6 +22,7 @@ enum class KtlintFeatureProfile(
     DISTRACT_FREE(
         setOf(
             FORMAT_WITH_KTLINT_ON_SAVE,
+            POST_FORMAT_WITH_KTLINT,
             // Although Ktlint is executed automatically on files being edited, this option is still useful for mass formatting on files
             // without having the need to edit each file individually.
             SHOW_MENU_OPTION_FORMAT_WITH_KTLINT,
@@ -45,6 +47,7 @@ enum class KtlintFeature {
     // | Highlight problems for all Ktlint violations (in open editor window) [automatically] | *no                              | *yes        | no            |
     // | Highlight problems for all Ktlint violations (in open editor window) [manually]      | *yes                             | *no         | yes           |
     // | Format with ktlint after normal format [automatically]                               | *yes                             | *no         | no            |
+    POST_FORMAT_WITH_KTLINT,
 
     /**
      * On save of a kotlin file, apply Ktlint formatting to the file if the corresponding on-save-action is enabled.
