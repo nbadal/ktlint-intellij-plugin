daa69e964941b1142e57e7bc72c8bf37b4886cd8
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2024-05-07 13:52:34 +0200

    Support publication of dev versions
    A dev-version is published on the new dev channel to the plugin. As plugin developer it becomes easier to keep using the last plugin version as it can be installed from Jebrains marketplace.
    
    The beta and dev version number are automatically expanded with a build timestamp to create unique and identifiable version numbers.

--- create-ktlint-plugin-zip.sh
+++ create-ktlint-plugin-zip.sh
@@ -8,8 +8,10 @@ function getGradleProperty {
     echo $PROPERTY_VALUE
 }
 
-if [ $(find plugin/build/idea-sandbox/plugins -name instrumented*.jar | wc -l | cut -w -f 2) -eq 1 ]; then
-  PLUGIN_VERSION=$(getGradleProperty "pluginVersion")
+INSTRUMENTED_JAR_FILE_NAME=$(find plugin/build/idea-sandbox/plugins -name instrumented*.jar)
+if [ $(echo ${INSTRUMENTED_JAR_FILE_NAME} | wc -l | cut -w -f 2) -eq 1 ]; then
+  INSTRUMENTED_JAR_FILE_NAME_WITHOUT_EXTENSION=${INSTRUMENTED_JAR_FILE_NAME%*.jar}
+  PLUGIN_VERSION=${INSTRUMENTED_JAR_FILE_NAME_WITHOUT_EXTENSION##*-plugin-}
   PLUGIN_ZIP_FILE_NAME=ktlint-plugin-${PLUGIN_VERSION}.zip
   (cd plugin/build/idea-sandbox/plugins && zip  -r ../../${PLUGIN_ZIP_FILE_NAME} ktlint)
   echo "Created ZIP file ./plugin/build/${PLUGIN_ZIP_FILE_NAME}"
