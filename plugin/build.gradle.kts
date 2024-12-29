eb483095154c156b1d50d175a55429fb2a1fbefc
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2024-12-29 15:41:15 +0100

    Upgrade to plugin infrastructure to Gradle Intellij Platform V2

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.konan.properties.Properties
 import java.io.FileInputStream
@@ -13,7 +14,7 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java") // Java support
     alias(libs.plugins.kotlin) // Kotlin support
-    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
@@ -25,8 +26,10 @@ plugins {
 //   group = properties("pluginGroup").get()
 //   version = properties("pluginVersion").get()
 val pluginGroup: String by project
+
 // `pluginName_` variable ends with `_` because of the collision with Kotlin magic getter in the `intellij` closure.
 // Read more about the issue: https://github.com/JetBrains/intellij-platform-plugin-template/issues/29
+@Suppress("PropertyName")
 val pluginName_: String by project
 val pluginVersion: String by project
 val pluginRepositoryUrl: String by project
@@ -51,12 +54,36 @@ version = finalPluginVersion
 
 repositories {
     mavenCentral()
+
+    // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+    intellijPlatform {
+        defaultRepositories()
+    }
+
     // Comment out next line releasing a new version to any channel
     // mavenLocal()
 }
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
+    testImplementation(libs.junit)
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+
+        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
+        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
+        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+
+        instrumentationTools()
+        pluginVerifier()
+        zipSigner()
+        testFramework(TestFrameworkType.Platform)
+    }
+
     compileOnly(project(":ktlint-lib")) // Required for IDE
     implementation(project(":ktlint-lib", "shadow"))
 
@@ -73,24 +100,100 @@ kotlin {
     jvmToolchain(17)
 }
 
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    // Replace due to multi-module setup
-    //   pluginName = properties("pluginName")
-    //   version = properties("platformVersion")
-    //   type = properties("platformType")
-    pluginName = pluginName_
-    version = platformVersion
-    type = platformType
+// Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+    pluginConfiguration {
+        // Read properties from variables due to multi-module setup
+        // Original
+        //   version = providers.gradleProperty("pluginVersion")
+        version = finalPluginVersion
 
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins =
-        // Replace due to multi-module setup
-        //   properties("platformPlugins")
-        //      .map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
-        // with
-        platformPlugins.split(',').map(String::trim).filter(String::isNotEmpty)
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        description =
+            providers
+                .fileContents(layout.projectDirectory.file("README.md"))
+                .asText
+                .map {
+                    val start = "<!-- Plugin description -->"
+                    val end = "<!-- Plugin description end -->"
+
+                    with(it.lines()) {
+                        if (!containsAll(listOf(start, end))) {
+                            throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                        }
+                        subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+                    }
+                }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes =
+            // Replace below with variable pluginVersion due to multi-module setup
+            // Original:
+            //     providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+
+        ideaVersion {
+            // Read properties from variables due to multi-module setup
+            // Original:
+            //     sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            //     untilBuild = providers.gradleProperty("pluginUntilBuild")
+            sinceBuild = pluginSinceBuild
+            untilBuild = pluginUntilBuild
+        }
+    }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        // The channel is set by setting the pluginVersion in the root `gradle.properties`
+        // Original:
+        //     channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+        channels =
+            listOfNotNull(
+                // Extract channel from `pluginVersion`. When version does not contain information that restricts the version to a specific
+                // channel, then it is to be published to the `default` channel.
+                finalPluginVersion.takeIfVersionForChannel("default"),
+                // Publish official and beta releases to beta channel
+                finalPluginVersion.takeIfVersionForChannel("beta"),
+                // Publish each version to the dev channel
+                "dev",
+            ).distinct()
+                .also { channels ->
+                    project.logger.lifecycle("PluginVersion `$finalPluginVersion` publishes the plugin to channels: $channels")
+                }
+    }
+
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
 }
+
+fun String.takeIfVersionForChannel(channel: String): String? =
+    split('-')
+        // Extract the channel from the version number. If no version is embedded in the version, assume the given channel was included in
+        // the version.
+        .getOrElse(1) { channel }
+        // The version can contain a subversion which is specified after a ".", and has to be ignored
+        .split('.')
+        .firstOrNull { it == channel }
 
 // Configure BuildConfig generation
 buildConfig {
@@ -112,14 +215,6 @@ changelog {
     // Replace due to multi-module setup
     // repositoryUrl = properties("pluginRepositoryUrl")
     repositoryUrl = pluginRepositoryUrl
-}
-
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath = provider { file(".qodana").canonicalPath }
-    reportPath = provider { file("build/reports/inspections").canonicalPath }
-    saveReport = true
-    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
@@ -158,8 +253,6 @@ tasks {
         // Read properties from variables due to multi-module setup
         // Original
         //   version = properties("pluginVersion")
-        //   sinceBuild = properties("pluginSinceBuild")
-        //   untilBuild = properties("pluginUntilBuild")
         version = finalPluginVersion
 
         sinceBuild = pluginSinceBuild
@@ -190,21 +283,12 @@ tasks {
             //      .map { pluginVersion ->
             with(changelog) {
                 renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
+                    (getOrNull(finalPluginVersion) ?: getUnreleased())
                         .withHeader(false)
                         .withEmptySections(false),
                     Changelog.OutputType.HTML,
                 )
             }
-    }
-
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
     }
 
     signPlugin {
@@ -214,33 +298,29 @@ tasks {
     }
 
     publishPlugin {
-        dependsOn("patchChangelog")
+        dependsOn(patchChangelog)
         token = environment("PUBLISH_TOKEN")
-        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel.
-        // The channel is set by setting the pluginVersion in the root `gradle.properties`
-        channels =
-            listOfNotNull(
-                // Extract channel from `pluginVersion`. When version does not contain information that restricts the version to a specific
-                // channel, then it is to be published to the `default` channel.
-                finalPluginVersion.takeIfVersionForChannel("default"),
-                // Publish official and beta releases to beta channel
-                finalPluginVersion.takeIfVersionForChannel("beta"),
-                // Publish each version to the dev channel
-                "dev",
-            ).distinct()
-                .also { channels ->
-                    project.logger.lifecycle("PluginVersion `$finalPluginVersion` publishes the plugin to channels: $channels")
-                }
     }
 }
 
-fun String.takeIfVersionForChannel(channel: String): String? =
-    split('-')
-        // Extract the channel from the version number. If no version is embedded in the version, assume the given channel was included in
-        // the version.
-        .getOrElse(1) { channel }
-        // The version can contain a subversion which is specified after a ".", and has to be ignored
-        .split('.')
-        .firstOrNull { it == channel }
+intellijPlatformTesting {
+    runIde {
+        register("runIdeForUiTests") {
+            task {
+                jvmArgumentProviders +=
+                    CommandLineArgumentProvider {
+                        listOf(
+                            "-Drobot-server.port=8082",
+                            "-Dide.mac.message.dialogs.as.sheets=false",
+                            "-Djb.privacy.policy.text=<!--999.999-->",
+                            "-Djb.consents.confirmation.enabled=false",
+                        )
+                    }
+            }
+
+            plugins {
+                robotServerPlugin()
+            }
+        }
+    }
+}
