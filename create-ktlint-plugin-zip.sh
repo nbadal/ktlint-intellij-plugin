73877fe3dce7d3e3b8b4cfad7ae1d6e8b5bb1c97
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-01-19 15:45:39 +0100

    Fix script create-ktlint-plugin-zip.sh (#623)
    Fix was broken due to migrating to Intellij Platform V2 plugin setup which changed internal build directory structure

--- create-ktlint-plugin-zip.sh
+++ create-ktlint-plugin-zip.sh
@@ -8,12 +8,13 @@ function getGradleProperty {
     echo $PROPERTY_VALUE
 }
 
-INSTRUMENTED_JAR_FILE_NAME=$(find ktlint-plugin/build/idea-sandbox/plugins -name instrumented*.jar)
+INSTRUMENTED_JAR_FILE_NAME=$(find ktlint-plugin/build/idea-sandbox/*/plugins -name ktlint-plugin*.jar)
 if [ $(echo ${INSTRUMENTED_JAR_FILE_NAME} | wc -l | cut -w -f 2) -eq 1 ]; then
+  PLUGINS_DIR=${INSTRUMENTED_JAR_FILE_NAME%/ktlint/lib/*}
   INSTRUMENTED_JAR_FILE_NAME_WITHOUT_EXTENSION=${INSTRUMENTED_JAR_FILE_NAME%*.jar}
   PLUGIN_VERSION=${INSTRUMENTED_JAR_FILE_NAME_WITHOUT_EXTENSION##*-plugin-}
   PLUGIN_ZIP_FILE_NAME=ktlint-plugin-${PLUGIN_VERSION}.zip
-  (cd ktlint-plugin/build/idea-sandbox/plugins && zip  -r ../../${PLUGIN_ZIP_FILE_NAME} ktlint)
+  (cd ${PLUGINS_DIR} && zip  -r ../../../${PLUGIN_ZIP_FILE_NAME} ktlint)
   echo "Created ZIP file ./ktlint-plugin/build/${PLUGIN_ZIP_FILE_NAME}"
 else
   echo "Can not create valid ZIP file. Run clean build first"
