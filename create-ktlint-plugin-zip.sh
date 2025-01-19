7763b26e0c243684e5d747a34809d6be62bde92a
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-01-19 14:52:14 +0100

    Refactor - Rename directory "plugin" to "ktlint-plugin"

--- create-ktlint-plugin-zip.sh
+++ create-ktlint-plugin-zip.sh
@@ -8,13 +8,13 @@ function getGradleProperty {
     echo $PROPERTY_VALUE
 }
 
-INSTRUMENTED_JAR_FILE_NAME=$(find plugin/build/idea-sandbox/plugins -name instrumented*.jar)
+INSTRUMENTED_JAR_FILE_NAME=$(find ktlint-plugin/build/idea-sandbox/plugins -name instrumented*.jar)
 if [ $(echo ${INSTRUMENTED_JAR_FILE_NAME} | wc -l | cut -w -f 2) -eq 1 ]; then
   INSTRUMENTED_JAR_FILE_NAME_WITHOUT_EXTENSION=${INSTRUMENTED_JAR_FILE_NAME%*.jar}
   PLUGIN_VERSION=${INSTRUMENTED_JAR_FILE_NAME_WITHOUT_EXTENSION##*-plugin-}
   PLUGIN_ZIP_FILE_NAME=ktlint-plugin-${PLUGIN_VERSION}.zip
-  (cd plugin/build/idea-sandbox/plugins && zip  -r ../../${PLUGIN_ZIP_FILE_NAME} ktlint)
-  echo "Created ZIP file ./plugin/build/${PLUGIN_ZIP_FILE_NAME}"
+  (cd ktlint-plugin/build/idea-sandbox/plugins && zip  -r ../../${PLUGIN_ZIP_FILE_NAME} ktlint)
+  echo "Created ZIP file ./ktlint-plugin/build/${PLUGIN_ZIP_FILE_NAME}"
 else
   echo "Can not create valid ZIP file. Run clean build first"
 fi
