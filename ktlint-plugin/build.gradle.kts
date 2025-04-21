bff8133bf0c058e68fc48a5f6c95948cbf813611
JiHwan Oh <hwan_@kakao.com>
2025-04-21 10:41:27 +0900

    Update build.gradle.kts

--- ktlint-plugin/build.gradle.kts
+++ ktlint-plugin/build.gradle.kts
@@ -131,7 +131,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
+            untilBuild = provider { null }
         }
     }
 
