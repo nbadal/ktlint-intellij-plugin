6b5636a9ad96fc95f4b0cfa7d0f28b1db0669f06
Nick Badal <me@nbad.al>
2026-01-24 20:48:07 -0800

    Include IDE version in update commit and pull request title within release check workflow

--- check-ide-release.sh
+++ check-ide-release.sh
@@ -8,7 +8,9 @@ fi
 PLUGIN_UNTIL_BUILD=$(grep pluginUntilBuild gradle.properties | cut -d '=' -f 2 | xargs)
 echo "Current plugin supports until build ${PLUGIN_UNTIL_BUILD}"
 
-LATEST_IDEA_BUILD=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=IIU&release.type=eap%2Crc&latest=true' | jq -r '.IIU[0].build')
+LATEST_IDEA_RESPONSE=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=IIU&release.type=eap%2Crc&latest=true')
+LATEST_IDEA_BUILD=$(echo "$LATEST_IDEA_RESPONSE" | jq -r '.IIU[0].build')
+LATEST_IDEA_VERSION=$(echo "$LATEST_IDEA_RESPONSE" | jq -r '.IIU[0].version')
 echo "Latest IDEA build ${LATEST_IDEA_BUILD}"
 
 if [[ "$LATEST_IDEA_BUILD" == "$PLUGIN_UNTIL_BUILD" ]]; then
@@ -34,6 +36,7 @@ fi
 
 echo "updated=true" >> $GITHUB_OUTPUT
 echo "newUntil=$UNTIL_VERSION" >> $GITHUB_OUTPUT
+echo "newVersion=$LATEST_IDEA_VERSION" >> $GITHUB_OUTPUT
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
     sed -i '' "s|pluginUntilBuild = $PLUGIN_UNTIL_BUILD|pluginUntilBuild = $UNTIL_VERSION|g" gradle.properties
