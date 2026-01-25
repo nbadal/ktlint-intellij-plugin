d4b80d1615667faaf42986aab1d2a52855fe65a3
Nick Badal <me@nbad.al>
2026-01-24 18:42:08 -0800

    Move duplicated ruleset logic to build-logic

---
+++ build-logic/src/main/kotlin/ktlint-ruleset.gradle.kts
@@ -0,0 +1,83 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.provider.Property
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+abstract class KtlintRulesetExtension {
+    abstract val version: Property<String>
+    abstract val includeKotlinxExcludes: Property<Boolean>
+    abstract val addEc4jCoreConstraint: Property<Boolean>
+}
+
+plugins {
+    id("java") // Java support
+    id("org.jetbrains.kotlin.jvm") // Kotlin support
+    id("com.gradleup.shadow")
+}
+
+repositories {
+    // Prevent that snapshot artifacts can be used for ktlint versions that have been released officially
+    mavenCentral()
+}
+
+val rulesetExtension =
+    extensions.create(
+        "ktlintRuleset",
+        KtlintRulesetExtension::class,
+    ).apply {
+        includeKotlinxExcludes.convention(false)
+        addEc4jCoreConstraint.convention(false)
+    }
+
+kotlin {
+    jvmToolchain(17)
+    compilerOptions {
+        apiVersion.set(KotlinVersion.KOTLIN_2_0)
+    }
+}
+
+afterEvaluate {
+    val ktlintVersion =
+        rulesetExtension.version.orNull
+            ?: error("ktlintRuleset.version is required")
+    val includeKotlinxExcludes = rulesetExtension.includeKotlinxExcludes.getOrElse(false)
+    val addEc4jCoreConstraint = rulesetExtension.addEc4jCoreConstraint.getOrElse(false)
+    val relocateSuffix = "V" + ktlintVersion.replace('.', '_')
+
+    dependencies {
+        implementation("com.pinterest.ktlint:ktlint-ruleset-standard:$ktlintVersion")
+
+        if (addEc4jCoreConstraint) {
+            constraints {
+                implementation("org.ec4j.core:ec4j-core:1.2.0") {
+                    because("Allows '.editorconfig' properties to be defined without any value")
+                }
+            }
+        }
+    }
+
+    tasks.withType<ShadowJar>().configureEach {
+        relocate(
+            "com.pinterest.ktlint.ruleset.standard",
+            "com.pinterest.ktlint.ruleset.standard.$relocateSuffix",
+        )
+
+        minimize {
+            exclude(dependency("com.pinterest.ktlint:ktlint-ruleset-standard:$ktlintVersion"))
+        }
+
+        // Cannot use the minimize-block as that would build a fat jar. The GitHub runner has too little diskspace to build the project if a
+        // fat jar is built for each version of the ktlint rulesets. Also, the non-ktlint dependencies will not be used in the final ktlint-lib
+        // jar as the files of the latest ruleset will be used instead.
+        exclude("dev/**")
+        exclude("gnu/**")
+        exclude("io/**")
+        exclude("javaslang/**")
+        exclude("kotlin/**")
+        if (includeKotlinxExcludes) {
+            exclude("kotlinx/**")
+        }
+        exclude("messages/**")
+        exclude("misc/**")
+        exclude("org/**")
+    }
+}
--- ktlint-lib/ruleset-1-1-1/build.gradle.kts
+++ ktlint-lib/ruleset-1-1-1/build.gradle.kts
@@ -1,51 +1,8 @@
 plugins {
-    id("java") // Java support
-    alias(libs.plugins.kotlin) // Kotlin support
-    alias(libs.plugins.shadow)
+    id("ktlint-ruleset")
 }
 
-// Prevent that snapshot artifacts can be used for ktlint versions that have been released officially
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("com.pinterest.ktlint:ktlint-ruleset-standard:1.1.1")
-
-    constraints {
-        // ec4-core version 0.3.0 which is included in ktlint 1.1.1 fails on '.editorconfig' properties without value
-        implementation("org.ec4j.core:ec4j-core:1.2.0") {
-            because("Allows '.editorconfig' properties to be defined without any value")
-        }
-    }
-}
-
-kotlin {
-    jvmToolchain(17)
-    compilerOptions {
-        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
-    }
-}
-
-tasks.shadowJar {
-    relocate(
-        "com.pinterest.ktlint.ruleset.standard",
-        "com.pinterest.ktlint.ruleset.standard.V1_1_1",
-    )
-
-    minimize {
-        exclude(dependency("com.pinterest.ktlint:ktlint-ruleset-standard:1.1.1"))
-    }
-
-    // Cannot use the minimize-block as that would build a fat jar. The GitHub runner has too little diskspace to build the project if a
-    // fat jar is built for each version of the ktlint rulesets. Also, the non-ktlint dependencies will not be used in the final ktlint-lib
-    // jar as the files of the latest ruleset will be used instead.
-    exclude("dev/**")
-    exclude("gnu/**")
-    exclude("io/**")
-    exclude("javaslang/**")
-    exclude("kotlin/**")
-    exclude("messages/**")
-    exclude("misc/**")
-    exclude("org/**")
+ktlintRuleset {
+    version = "1.1.1"
+    addEc4jCoreConstraint = true
 }
