0f6fa1af4753f76651180bdb8fdd863872825d95
haysmike <mike.hays@goswift.ly>
2026-02-01 11:30:35 -0800

    Update comment about relocations

--- ktlint-lib/build.gradle.kts
+++ ktlint-lib/build.gradle.kts
@@ -97,7 +97,7 @@ tasks {
     withType<ShadowJar> {
         // Relocate to prevent conflicts with same packages provided by Intellij IDEA as well. The embeddable Kotlin compiler in the Ktlint
         // jar differs from the compiler provided in the IDEA.
-        // IMPORTANT: Third party suppliers of rule set need to add those relocations as well!
+        // IMPORTANT: These need to be added to `RelocatingClassLoader` as well!
         relocate("org.jetbrains.kotlin.psi.KtPsiFactory", "shadow.org.jetbrains.kotlin.psi.KtPsiFactory")
         relocate("org.jetbrains.kotlin.psi.psiUtil", "shadow.org.jetbrains.kotlin.psi.psiUtil")
 
@@ -115,7 +115,7 @@ tasks {
         //       Bundling IDE packages is considered bad practice and may lead to sophisticated compatibility problems. Consider excluding
         //       these IDE packages from the plugin distribution. If your plugin depends on classes of an IDE bundled plugin, explicitly
         //       specify dependency on that plugin instead of bundling it.
-        // IMPORTANT: Third party suppliers of rule set need to add those relocations as well!
+        // IMPORTANT: These need to be added to `RelocatingClassLoader` as well!
         relocate("org.jetbrains.org", "shadow.org.jetbrains.org")
         relocate("org.jetbrains.concurrency", "shadow.org.jetbrains.concurrency")
         relocate("org.jetbrains.kotlin", "shadow.org.jetbrains.kotlin")
